7e54e4f8abfb3502d0b5e7ba3c03897c7b514414
vince <vincevermeersch@gmail.com>
2026-06-11 12:41:41 +0200

    SpringWeb TEST day 4: Small changes & fixes

--- src/main/resources/static/js/succes.js
+++ src/main/resources/static/js/succes.js
@@ -3,13 +3,9 @@ const infoAlert = document.getElementById('info-alert');
 const bestelIdLabel = document.getElementById("bestel-id");
 const navBiermandje = document.getElementById('nav-biermandje');
 
-async function biermandjeExists() {
-    return null === sessionStorage.getItem("biermandje");
-}
-
 window.onload = async () => {
     const bestelId = sessionStorage.getItem('bestelId');
     infoAlert.hidden = false;
     bestelIdLabel.textContent = bestelId;
-    navBiermandje.hidden = await biermandjeExists();
+    navBiermandje.hidden = null === sessionStorage.getItem("biermandje");
 }
